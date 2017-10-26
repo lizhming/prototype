bc3f280f73309e88ce31d5d4a85ff813dc53e5e3
Rachit Shrivastava <rshriva@ncsu.edu>
2017-10-26 14:49:40 -0400

    right screen click handler

--- v2.0/js/main.js
+++ v2.0/js/main.js
@@ -6,8 +6,6 @@ function init() {
 	canvas = document.getElementById("testCanvas");
 	stage = new createjs.Stage(canvas);
 
-	maintainAspectRatio();
-
 	// enable touch interactions if supported on the current device:
 	stage.autoClear = true;
 	createjs.Touch.enable(stage);
@@ -16,4 +14,30 @@ function init() {
 	// enabled mouse over / out events
 	stage.enableMouseOver(10);
 	stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas
+
+	resize();
+
+	createjs.Ticker.addEventListener("tick", tick);
+
+	makeContainers();
+}
+
+function tick(event) {
+	stage.update();
+}
+
+function makeContainers() {
+	var hitArea = new createjs.Shape();
+	hitArea.graphics.f("#000").drawRect(0, 0, w, h);
+	var detail = new createjs.Container();
+	detail.setTransform(w/2, 0);
+	detail.setBounds(0, 0, w, h);
+	detail.hitArea = hitArea;
+	stage.addChild(detail);
+
+	detail.on("click", clickHandler);
+}
+
+function clickHandler(e) {
+	console.log(e.rawX +" "+e.rawY);
 }
