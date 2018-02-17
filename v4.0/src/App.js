26fc35fb9985a4a609692900e7f2f6e1183cd536
Rachit Shrivastava <rshriva@ncsu.edu>
2018-02-16 21:39:35 -0500

    calculating aspect ratio

--- v4.0/src/App.js
+++ v4.0/src/App.js
@@ -2,12 +2,48 @@ import React, { Component } from 'react';
 import Cards from './Cards.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this._maintainAspectRatio = this._maintainAspectRatio.bind(this);
+    this.state = {
+      w: 1920, 
+      h: 1080,
+      ratio: 0
+    };
+    this._isMounted = false;
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    window.addEventListener('resize', this._maintainAspectRatio, false);
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+    window.removeEventListener('resize', this._maintainAspectRatio, false);
+  }
+
+  _maintainAspectRatio() {
+    if (this._isMounted) {
+      this.setState({ ratio: window.innerWidth / this.state.w });
+      if (this.state.h * this.state.ratio > window.innerHeight) {
+          this.setState({ ratio: window.innerHeight / this.state.h });
+      }
+
+      this.setState({ 
+        w: this.state.w * this.state.ratio, 
+        h: this.state.h * this.state.ratio 
+      });
+
+      console.log("Aspect Ratio " + this.state.ratio);
+    }
+  }
 
   render() {
     const viz = 'Viz';
     const rater = 'Rater Agreement';
     const dash = 'DashBoard';
-    
+
     return (
       <div className="container-fluid">
         <div className="row"> 
