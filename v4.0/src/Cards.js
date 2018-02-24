4651b8a25921c5e112c3d90c4edd90b2c89e610d
Rachit Shrivastava <rshriva@ncsu.edu>
2018-02-24 00:05:22 -0500

    classification areas

--- v4.0/src/Cards.js
+++ v4.0/src/Cards.js
@@ -11,36 +11,70 @@ class Cards extends React.Component {
 
     this.state = { 
       activeDrags : 0,
-      deltaPosition: {x: 0, y: 0},
-      controlledPosition: {x: 0, y: 0},
+      deltaPosition: {x: 0, y: 0}
     };
+
+    const no_of_cards = 10;
+    const qcard = "Q-Card";
+    const dragHandlers = {
+      onDrag: this.handleDrag, 
+      onStart: this.onStart, 
+      onStop: this.onStop
+    };
+
+    this.cards = [];
+    for(var i=0; i<no_of_cards; i++) {
+      //put cards in random location between x & y betwwen (275, 475)
+      var px = this.props.ratio * (Math.floor(Math.random() * 248) + 340);
+      var py = this.props.ratio * (Math.floor(Math.random() * 200) + 275);
+      var bh = (this.props.ratio * this.size) + "px";
+
+      //console.log(i, px, py);
+
+      this.cards.push(
+        <Draggable key={i} defaultPosition={{x: px, y: py}} bounds="parent" {...dragHandlers}>
+            <div className="box" style={{height: bh, width: bh}}>{qcard}-{i}</div>
+        </Draggable>);
+    }
   }
 
   createBindings() {
     this.onStart = this.onStart.bind(this);
     this.onStop = this.onStop.bind(this);
     this.handleDrag = this.handleDrag.bind(this);
-    this.adjustXPos = this.adjustXPos.bind(this);
-    this.adjustYPos = this.adjustYPos.bind(this);
-    this.onControlledDrag = this.onControlledDrag.bind(this);
-    this.onControlledDragStop = this.onControlledDragStop.bind(this);
   }
 
   handleDrag(e, ui) {
     const {x, y} = this.state.deltaPosition;
-    //console.log(e, this.state.deltaPosition);
     this.setState({
       deltaPosition: {
         x: x + ui.deltaX,
-        y: y + ui.deltaY,
+        y: y + ui.deltaY
       }
     });
-    //console.log(this.state.deltaPosition);
-    document.getElementsByClassName("react-draggable-dragging")[0].style.zIndex = this.val++;
-    this.toggleSize(false);
+    
+    var elem = document.getElementsByClassName("react-draggable-dragging")[0];
+    elem.style.zIndex = this.val;
+    //elem.innerHTML = this.state.deltaPosition.x + ", " + this.state.deltaPosition.y;
+    this.toggleSize(elem, false);
+    this.locateCard(elem, ui.x, ui.y);
   }
 
-  onStart() {
+  locateCard(elem, x, y) {
+    if(x >= 275 && x <= 475 && y >= 275 && y <= 475) {
+      elem.style.background = "#fff";
+    } else if(x >= 600 && y <= 600) {  //relevant
+      elem.style.background = "#0f0";
+    } else if(x <= 150 && y <= 600) { //irrelevant
+      elem.style.background = "#f00";
+    } else if(y > 600) {  //unknown
+      elem.style.background = "#00f";
+    }
+  }
+
+  onStart(e, ui) {
+    console.log("onStart", ui.x, ui.y);
+    ++this.val;
     this.setState({
       activeDrags: this.state.activeDrags + 1
     });
@@ -48,72 +82,28 @@ class Cards extends React.Component {
 
   onStop() {
     this.setState({
-      activeDrags: this.state.activeDrags - 1
+      activeDrags: this.state.activeDrags - 1,
+      deltaPosition: {x: 0, y: 0}
     });
-    this.toggleSize(false);
+    this.toggleSize(document.getElementsByClassName("react-draggable-dragging")[0], false);
+
   }
 
-  // For controlled component
-  adjustXPos(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const {x, y} = this.state.controlledPosition;
-    this.setState({controlledPosition: {x: x - 10, y}});
-  }
-
-  adjustYPos(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const {controlledPosition} = this.state;
-    const {x, y} = controlledPosition;
-    this.setState({controlledPosition: {x, y: y - 10}});
-  }
-
-  onControlledDrag(e, position) {
-    const {x, y} = position;
-    this.setState({controlledPosition: {x, y}});
-  }
-
-  onControlledDragStop(e, position) {
-    this.onControlledDrag(e, position);
-    this.onStop();
-  }
-
-  toggleSize(flg) {
+  toggleSize(elem, flg) {
     var factor = 1;
     if(flg) {
       factor = 0.52;
     }
-    document.getElementsByClassName("react-draggable-dragging")[0].style.height = (this.size * factor) + "px";
-    document.getElementsByClassName("react-draggable-dragging")[0].style.width = (this.size * factor) + "px";
+    elem.style.height = (this.size * factor) + "px";
+    elem.style.width = (this.size * factor) + "px";
   }
 
   render() {
-    const no_of_cards = 15;
-    const qcard = "Q-Card";
-    const dragHandlers = {
-      onDrag: this.handleDrag, 
-      onStart: this.onStart, 
-      onStop: this.onStop
-    };
-    
     const h = this.props.ratio * (window.innerHeight-66) + "px";
-
-    var cards = [];
-    for(var i=0; i<no_of_cards; i++) {
-      var px = this.props.ratio * (Math.floor(Math.random() * 300) + 200);
-      var py = this.props.ratio * (Math.floor(Math.random() * 275) + 175);
-      var bh = (this.props.ratio * this.size) + "px";
-
-      cards.push(
-        <Draggable key={i} defaultPosition={{x: px, y: py}} bounds="parent" {...dragHandlers}>
-            <div className="box" style={{height: bh, width: bh}}>{qcard}</div>
-        </Draggable>);
-    }
 
     return (
       <div id="main" style={{position:"relative", height:h, width:"100%"}}>
-        {cards}
+        {this.cards}
       </div>
     );
   }
